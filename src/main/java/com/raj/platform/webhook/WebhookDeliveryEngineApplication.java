2a77a79ea19c21c2b792b1c1fc56e0d33658e4f6
Raj Shah <rajshah11112003@gmail.com>
2026-02-03 23:35:27 +0530

    feat: updated application.yml file and add enable kafka annotation

--- src/main/java/com/raj/platform/webhook/WebhookDeliveryEngineApplication.java
+++ src/main/java/com/raj/platform/webhook/WebhookDeliveryEngineApplication.java
@@ -2,8 +2,10 @@ package com.raj.platform.webhook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class WebhookDeliveryEngineApplication {
 
 	public static void main(String[] args) {
